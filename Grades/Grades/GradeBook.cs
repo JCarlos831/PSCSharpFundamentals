5b382277b3203de6575da5bd0c5eadf341ad86c1
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-12-26 13:10:56 -0500

    Classes and Objects

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            _grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+            float sum = 0;
+
+            foreach (float grade in _grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / _grades.Count;
+
+            return stats;
+        }
+        
+        public void AddGrade(float grade)
+        {
+            _grades.Add(grade);
+        }
+
+        private readonly List<float> _grades;
+    }
+}
